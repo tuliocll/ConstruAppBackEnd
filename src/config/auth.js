207bb43f019c72d44a78f3bb88d1b1e989b69f35
tulio <tuliocll@gmail.com>
2019-09-03 19:04:57 -0300

    adicionar dotenv e env-example

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: "492c36f248f2a1f020974150fb2d7ba4",
+  secret: process.env.APP_SECRET,
   expiresIn: "7d"
 };
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require("dotenv/config");
+
 module.exports = {
   dialect: "postgres",
-  username: "zbsmwkrswkfdmf",
-  password: "20094cce6d64cb28e70cba1c3a0c283541b82c9360dbf49097e03b1029e9ff14",
-  database: "dbn8ts0bmcvs9j",
-  host: "ec2-23-21-156-171.compute-1.amazonaws.com",
+  host: process.env.DB_HOST,
+  username: process.env.DB_HOST,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
